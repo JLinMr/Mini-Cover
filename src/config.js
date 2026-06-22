535ae45ac0e3c195b7d6bfa5ebdaca838d164ac5
JLinmr <power916@163.com>
2026-06-22 13:56:21 +0800

    ✨ 更新字体 CDN 配置

--- src/config.js
+++ src/config.js
@@ -3,10 +3,10 @@ const fonts = [
     ['Microsoft YaHei', '微软雅黑'],
     // [字体名称, 显示名称, CSS文件地址]
     ['HarmonyOS_Regular', '鸿蒙字体', 'https://s1.hdslb.com/bfs/static/jinkela/long/font/regular.css'],
-    ['yozai', '悠哉字体', 'https://chinese-fonts-cdn.deno.dev/packages/yozai/dist/Yozai-Regular/result.css'],
-    ['寒蝉全圆体', '寒蝉全圆体', 'https://chinese-fonts-cdn.deno.dev/packages/hcqyt/dist/ChillRoundFRegular/result.css'],
-    ['Douyin Sans', '抖音美好体', 'https://chinese-fonts-cdn.deno.dev/packages/dymh/dist/DouyinSansBold/result.css'],
-    ['MaokenZhuyuanTi', '猫啃珠圆体', 'https://chinese-fonts-cdn.deno.dev/packages/mkzyt/dist/猫啃珠圆体/result.css']
+    ['yozai', '悠哉字体', 'https://cn-font.claude-code-best.win/packages/yozai/dist/Yozai-Regular/result.css'],
+    ['寒蝉全圆体', '寒蝉全圆体', 'https://cn-font.claude-code-best.win/packages/hcqyt/dist/ChillRoundFRegular/result.css'],
+    ['Douyin Sans', '抖音美好体', 'https://cn-font.claude-code-best.win/packages/dymh/dist/DouyinSansBold/result.css'],
+    ['MaokenZhuyuanTi', '猫啃珠圆体', 'https://cn-font.claude-code-best.win/packages/mkzyt/dist/猫啃珠圆体/result.css']
 ];
 
 export const defaultConfig = {
